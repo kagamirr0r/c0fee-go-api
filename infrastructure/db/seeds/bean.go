c9fe96158f59891f738ba0ee48fd93998ab4ba9f
Kagamirr0r <a7112069@gmail.com>
2025-02-15 23:42:46 +0900

    refactor: update bean seeds to support dynamic user ID and image file extensions

--- infrastructure/db/seeds/bean.go
+++ infrastructure/db/seeds/bean.go
@@ -2,8 +2,9 @@ package seeds
 
 import (
 	"c0fee-api/model"
-	"fmt"
+	"os"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -11,66 +12,71 @@ func CreateBeanSeeds(db *gorm.DB) error {
 
 	// ユーザーを取得
 	var user model.User
-	if err := db.First(&user).Error; err != nil {
-		return err
+	var userId uuid.UUID
+
+	if testID := os.Getenv("TEST_USER_ID"); testID == "" {
+		if err := db.First(&user).Error; err == nil {
+			userId = user.ID
+		}
+	} else {
+		userId = uuid.MustParse(testID)
 	}
-	fmt.Print(user)
 
 	beans := []model.Bean{
 		{
 			Name:            "Ethiopian Yirgacheffe",
-			UserID:          user.ID,
+			UserID:          userId,
 			RoasterID:       1,
 			ProcessMethodID: 1,
 			Countries:       []model.Country{{Name: "Ethiopia", Code: "ET"}},
 			Varieties:       []model.Variety{{Variety: "Heirloom"}},
 			Area:            "Yirgacheffe",
 			RoastLevel:      model.Medium,
-			ImageKey:        "1/image.jpg",
+			ImageKey:        "1/image.png",
 		},
 		{
 			Name:            "Colombian Supremo",
-			UserID:          user.ID,
+			UserID:          userId,
 			RoasterID:       2,
 			ProcessMethodID: 2,
 			Countries:       []model.Country{{Name: "Colombia", Code: "CO"}},
 			Varieties:       []model.Variety{{Variety: "Caturra"}},
 			Area:            "Antioquia",
 			RoastLevel:      model.Dark,
-			ImageKey:        "2/image.jpg",
+			ImageKey:        "2/image.png",
 		},
 		{
 			Name:            "Kenyan AA",
-			UserID:          user.ID,
+			UserID:          userId,
 			RoasterID:       3,
 			ProcessMethodID: 3,
 			Countries:       []model.Country{{Name: "Kenya", Code: "KE"}},
 			Varieties:       []model.Variety{{Variety: "SL28"}},
 			Area:            "Nyeri",
 			RoastLevel:      model.Light,
-			ImageKey:        "3/image.jpg",
+			ImageKey:        "3/image.png",
 		},
 		{
 			Name:            "Sumatra Mandheling",
-			UserID:          user.ID,
+			UserID:          userId,
 			RoasterID:       4,
 			ProcessMethodID: 4,
 			Countries:       []model.Country{{Name: "Indonesia", Code: "ID"}},
 			Varieties:       []model.Variety{{Variety: "Typica"}},
 			Area:            "Aceh",
 			RoastLevel:      model.MediumDark,
-			ImageKey:        "4/image.jpg",
+			ImageKey:        "4/image.png",
 		},
 		{
 			Name:            "Guatemalan Antigua",
-			UserID:          user.ID,
+			UserID:          userId,
 			RoasterID:       5,
 			ProcessMethodID: 5,
 			Countries:       []model.Country{{Name: "Guatemala", Code: "GT"}},
 			Varieties:       []model.Variety{{Variety: "Bourbon"}},
 			Area:            "Antigua",
 			RoastLevel:      model.MediumLight,
-			ImageKey:        "5/image.jpg",
+			ImageKey:        "5/image.png",
 		},
 	}
 
