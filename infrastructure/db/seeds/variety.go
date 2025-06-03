d6d07ce85c0cf74b8ac047a3342be5963aad1019
Kagamirr0r <a7112069@gmail.com>
2025-06-03 20:49:59 +0900

    refactor: rename Variety field to Name and update related structures

--- infrastructure/db/seeds/variety.go
+++ infrastructure/db/seeds/variety.go
@@ -8,18 +8,18 @@ import (
 
 func CreateVarietySeeds(db *gorm.DB) error {
 	varieties := []model.Variety{
-		{ID: 1, Variety: "Heirloom"},
-		{ID: 2, Variety: "Typica"},
-		{ID: 3, Variety: "Bourbon"},
-		{ID: 4, Variety: "Catuai"},
-		{ID: 5, Variety: "SL34"},
-		{ID: 6, Variety: "SL28"},
-		{ID: 7, Variety: "Ruiru11"},
-		{ID: 8, Variety: "Pacamara"},
-		{ID: 9, Variety: "Mundo Novo"},
-		{ID: 10, Variety: "Chiroso"},
-		{ID: 11, Variety: "Geisha"},
-		{ID: 12, Variety: "Caturra"},
+		{ID: 1, Name: "Heirloom"},
+		{ID: 2, Name: "Typica"},
+		{ID: 3, Name: "Bourbon"},
+		{ID: 4, Name: "Catuai"},
+		{ID: 5, Name: "SL34"},
+		{ID: 6, Name: "SL28"},
+		{ID: 7, Name: "Ruiru11"},
+		{ID: 8, Name: "Pacamara"},
+		{ID: 9, Name: "Mundo Novo"},
+		{ID: 10, Name: "Chiroso"},
+		{ID: 11, Name: "Geisha"},
+		{ID: 12, Name: "Caturra"},
 	}
 
 	for _, variety := range varieties {
--- model/variety.go
+++ model/variety.go
@@ -8,8 +8,20 @@ import (
 
 type Variety struct {
 	ID        uint           `json:"id" param:"id" gorm:"primary_key;" validate:"required"`
-	Variety   string         `json:"variety" gorm:"unique" validate:"required"`
+	Name      string         `json:"name" gorm:"unique" validate:"required"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
+}
+
+type VarietyResponse struct {
+	ID    uint   `json:"id" param:"id"`
+	Name  string `json:"name"`
+	Code  string `json:"code"`
+	Areas []Area `json:"areas"`
+}
+
+type VarietiesResponse struct {
+	Countries []CountryResponse `json:"countries"`
+	Count     uint              `json:"count"`
 }
